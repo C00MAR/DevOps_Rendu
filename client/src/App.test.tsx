95533edf40384ae19bb4dd642975e5268ae5c251
unknown <moureau.marc78@gmail.com>
2025-07-25 00:15:01 +0200

    :construction: Testing File :construction:

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App', () => {
-  render(<App />);
-  const paragraphElement = screen.getByRole('paragraph');
-  expect(paragraphElement).toBeInTheDocument();
-});
